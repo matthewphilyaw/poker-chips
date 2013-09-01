99394adfd1d41fa4cbabb5b6f41fcafc0bf7d2b0
matthew philyaw <matthew.philyaw@gmail.com>
2013-09-01 18:36:58 -0400

    reworked it to work off of std in.

--- src/poker_chips/core.clj
+++ src/poker_chips/core.clj
@@ -1,16 +1,8 @@
 (ns poker-chips.core
-  (:use [clojure.tools.cli :only (cli)]))
+  (:use [clojure.tools.cli :only (cli)]
+        poker-chips.parser
+        poker-chips.chips))
 
 (defn -main [& args]
-  (let [[opts args banner]
-        (cli args
-             ["-h" "--help" "show help" :flag true :default false]
-             ["-bi" "--buy-in" "buy in amount" :flag false]
-             ["-nhc" "--num-high-chip" "number of high chips" :flag false]
-             ["-s" "--step" "step between chips" :flag false])]
-    (or (and (or (:help opts) 
-                 (not (and (:buy-in opts) 
-                           (:num-high-chip opts)
-                           (:step opts))))
-             (do (println banner) true))
-        (println opts))))
+  (doseq [line (line-seq (java.io.BufferedReader. *in*))] 
+    (prn (process-chips (parse-line line)))))
